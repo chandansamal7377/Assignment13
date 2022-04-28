d794f24474aded449ecdf73761d101ca131f1083
chandansamal7377 <98443276+chandansamal7377@users.noreply.github.com>
2022-04-28 14:25:52 +0530

    Update Uc1.java

--- Uc1.java
+++ Uc1.java
@@ -1,7 +1,7 @@
 public class Uc1 {
   // determines the largest of three Comparable objects
-  public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
-    T max = x; // assume x is initially the largest
+  public static <G extends Comparable<G>> G maximum(G x, G y, G z) {
+    G max = x; // assume x is initially the largest
 
     if (y.compareTo(max) > 0)
       max = y; // y is the largest so far
@@ -14,6 +14,5 @@ public class Uc1 {
 
   public static void main(String args[]) {
     System.out.print("Maximum of %d, %d and %d is %d\n\n", 3, 4, 5, maximum(3, 4, 5));
-    
   }
 }
